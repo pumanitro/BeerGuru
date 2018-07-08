a2cd615b0474bd41cb706c367136cbbc056e748d
pumanitro <pumanitro@gmail.com>
2018-07-08 13:35:48 +0200

    Added react devtools configuration to the app.

--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
@@ -7,7 +8,7 @@ import CombinedReducers from './redux/reducers/index';
 
 import Root from './config/Root';
 
-const store = createStore(CombinedReducers);
+const store = createStore(CombinedReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({name: 'Netgru - Beer App'}));
 
 const render = (Component) => {
     ReactDOM.render(
