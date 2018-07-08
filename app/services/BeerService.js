de1613c409cc0af8743123de3da0ca321c560aa1
pumanitro <pumanitro@gmail.com>
2018-07-08 18:39:18 +0200

    Created simple service for retrieving beers from API.

--- app/services/BeerService.js
+++ app/services/BeerService.js
@@ -1,7 +1,18 @@
 class BeerService {
 
-    test() {
-        console.warn('TEST BEER');
+    constructor() {
+        this.API_CONSTS = {
+            ROOT_ENDPOINT: 'https://api.punkapi.com/v2/beers',
+            BEERS_PER_PAGE: 20
+        };
+    }
+
+    getBeers(page, beersPerPage) {
+        return fetch(`${this.API_CONSTS.ROOT_ENDPOINT}?page=${page}&per_page=${beersPerPage}`);
+    }
+
+    getMoreBeers(page) {
+        return this.getBeers(page, this.API_CONSTS.BEERS_PER_PAGE);
     }
 
 }
