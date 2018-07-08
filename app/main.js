b3bcda1d4e3614e7c7463757edaba936616ff263
pumanitro <pumanitro@gmail.com>
2018-07-08 13:57:37 +0200

    Installed sagas and connected it with store.

--- app/main.js
+++ app/main.js
@@ -3,12 +3,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import CombinedReducers from './redux/reducers/index';
+import createSagaMiddleware from 'redux-saga';
 
 import Root from './config/Root';
 
-const store = createStore(CombinedReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({name: 'Netgru - Beer App'}));
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancers = process.env.NODE_ENV !== 'production'
+    && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        name: 'Netgru - Beer App'
+    }) : compose;
+
+const store = createStore(CombinedReducers, composeEnhancers(applyMiddleware(sagaMiddleware)));
 
 const render = (Component) => {
     ReactDOM.render(
