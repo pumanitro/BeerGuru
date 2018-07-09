c67d10514a290ecb15206ad018ddb7f2313e71a7
pumanitro <pumanitro@gmail.com>
2018-07-09 23:19:40 +0200

    Made from beer component button and dispatch openModal action every click.

--- app/components/BearList/Beer/Beer.js
+++ app/components/BearList/Beer/Beer.js
@@ -1,12 +1,16 @@
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import styles from './Beer.scss';
+import {ActionTypes} from '../../../redux/actions/ActionTypes';
+import {makeAction} from '../../../redux/actions/ActionCreator';
 
-const Beer = ({beer}) => {
+const Beer = ({beer, openModal}) => {
 
     return (
-        <div className={styles['beer__container']}>
+        // div with role button and onKeyPress for people with physical disabilities
+        <div role="button" tabIndex={0} className={styles['beer__container']} onClick={() => openModal(beer.id)} onKeyPress={() => openModal(beer.id)}>
             <img className={styles['beer__img']} src={beer.image_url} alt={`Beer - ${beer.name}`} />
             <div className={styles['beer__name']}>{beer.name}</div>
             <div className={styles['beer__tagline']}>{beer.tagline}</div>
@@ -15,7 +19,13 @@ const Beer = ({beer}) => {
 };
 
 Beer.propTypes = {
-    beer: PropTypes.object.isRequired
+    beer: PropTypes.object.isRequired,
+
+    openModal: PropTypes.func.isRequired
 };
 
-export default Beer;
+const mapDispatchToProps = {
+    openModal: makeAction(ActionTypes.beerModal.OPEN_BEER_MODAL)
+};
+
+export default connect(null, mapDispatchToProps)(Beer);
