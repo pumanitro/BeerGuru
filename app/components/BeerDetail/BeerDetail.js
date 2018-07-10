54c328ae3fcb5be4370c0602aef886380217d521
pumanitro <pumanitro@gmail.com>
2018-07-10 21:31:40 +0200

    Showing more details about the beer.

--- app/components/BeerDetail/BeerDetail.js
+++ app/components/BeerDetail/BeerDetail.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import styles from './BeerDetail.scss';
 
 const BeerDetails = ({beer}) => {
 
     return (
-        <div>
-            <h3>{beer.name}</h3>
+        <div className={styles['beer-detail__container']}>
+            <img className={styles['beer-detail__img']} src={beer.image_url} alt={`Beer - ${beer.name}`} />
+            <div>
+                <h2>{beer.name}</h2>
+                <h3>{beer.tagline}</h3>
+                <div>
+                    <span>IBU: {beer.ibu}</span>
+                    <span>ABV: {beer.abv}</span>
+                    <span>EBC: {beer.ebc}</span>
+                </div>
+                <h4>Description:</h4>
+                <p>{beer.description}</p>
+                <h4>Brewer tips:</h4>
+                <p>{beer.brewers_tips}</p>
+            </div>
         </div>
     );
 };
