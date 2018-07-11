24e2874c2987ef5cf1e92e6c1164460f66d62787
pumanitro <pumanitro@gmail.com>
2018-07-11 22:33:02 +0200

    Handling situations when you can't retrieve similar beer.

--- app/components/BeerModal/BeerDetails/SimilarBeers/SimilarBeer/SimilarBeer.js
+++ app/components/BeerModal/BeerDetails/SimilarBeers/SimilarBeer/SimilarBeer.js
@@ -4,6 +4,10 @@ import styles from './SimilarBeer.scss';
 
 const SimilarBeer = ({beer}) => {
 
+    if (!beer) {
+        return <div className={styles['similar-beer__img']}> {"Can't get similar beer for it"}</div>;
+    }
+
     return (
         <img className={styles['similar-beer__img']} src={beer.image_url} alt={`Beer - ${beer.name}`} />
     );
